1c58bff1313e831da157d8e06d5bab9f7b2b51c0
Jefferson Raga Salunga <jefferson.salunga@yahoo.com>
2016-02-15 16:23:47 +0800

    add users table seeder

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -28,6 +28,25 @@ class UsersTableSeeder extends Seeder
             $this->command->info($branch->descriptor);
         }
 
+
+        $branches = Branch::all();
+
+        $users = [];
+        foreach ($branches as $branch) {
+
+            array_push($users, [
+                'username' => strtolower($branch->code).'-manager',
+                'name' => $branch->code.' Manager',
+                'branchid'=> $branch->id,
+                'email' => strtolower($branch->code).'-manager@giligansrestaurant.com',
+                'password' => bcrypt('giligans'),
+                //'admin'=>5,
+                'id' => $branch->id
+            ]);
+            $this->command->info(strtolower($branch->code).'-manager');
+        }
+        DB::table('user')->insert($users);
+
         DB::table('users')->insert($users);
 
         $this->command->info('Users table seeded!');
